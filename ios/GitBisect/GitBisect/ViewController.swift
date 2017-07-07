1aceef9fa239bab3f8820be128f4721bfb700f0a
Alex Curran <aml.curran@gmail.com>
2017-07-07 15:58:18 +0200

    Initial iOS commit

---
+++ ios/GitBisect/GitBisect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitBisect
+//
+//  Created by Alex on 07/07/2017.
+//  Copyright © 2017 AutoScout24 GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
