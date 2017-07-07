135a5c993b3b86d5ce2e9612b759be4b37504803
Alex Curran <aml.curran@gmail.com>
2017-07-07 15:12:08 +0200

    Adds a basic list view

--- android/app/src/main/java/uk/co/amlcurran/bisectapp/MainActivity.kt
+++ android/app/src/main/java/uk/co/amlcurran/bisectapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package uk.co.amlcurran.bisectapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById(R.id.recycler) as RecyclerView
+        recyclerView.adapter = BasicAdapter()
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
