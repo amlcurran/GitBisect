74a90709235f2601248f3be3273f7de8202b38e1
Alex Curran <aml.curran@gmail.com>
2017-07-07 16:10:41 +0200

    Pull out a collaborator

---
+++ ios/GitBisect/GitBisect/FruitDataSource.swift
@@ -0,0 +1,30 @@
+//
+//  FruitDataSource.swift
+//  GitBisect
+//
+//  Created by Alex on 07/07/2017.
+//  Copyright © 2017 AutoScout24 GmbH. All rights reserved.
+//
+
+import UIKit
+
+class FruitDataSource: NSObject, UITableViewDataSource {
+
+    private var fruits: [Fruit]
+
+    init(fruits: [Fruit]) {
+        self.fruits = fruits
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Foo", for: indexPath)
+        let fruit = fruits[indexPath.row]
+        cell.textLabel?.text = "\(fruit.name)"
+        return cell
+    }
+
+}
--- ios/GitBisect/GitBisect/ViewController.swift
+++ ios/GitBisect/GitBisect/ViewController.swift
@@ -11,17 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    let fruits = [
+    static let fruits = [
         Fruit(name: "Banana", price: 30, color: "Yellow", weight: nil),
         Fruit(name: "Apple", price: 10, color: "Green", weight: nil),
         Fruit(name: "Apple", price: 13, color: "Red", weight: nil),
         Fruit(name: "Watermelon", price: 130, color: "Green", weight: nil)
     ]
+    let dataSource = FruitDataSource(fruits: ViewController.fruits)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(BasicCell.self, forCellReuseIdentifier: "Foo")
-        tableView.dataSource = self
+        tableView.dataSource = dataSource
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,21 +30,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-}
-
-extension ViewController: UITableViewDataSource {
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fruits.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Foo", for: indexPath)
-        let fruit = fruits[indexPath.row]
-        cell.textLabel?.text = "\(fruit.name)"
-        return cell
-    }
 
 }
 
